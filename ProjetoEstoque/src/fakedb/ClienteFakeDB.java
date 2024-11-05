4931e5ab4452a125b32c334e63103f765cf0fc78
mcabral62 <cabralgg00@gmail.com>
2024-11-05 18:12:39 -0400

    Add files via upload
    correção.

--- ProjetoEstoque/src/fakedb/ClienteFakeDB.java
+++ ProjetoEstoque/src/fakedb/ClienteFakeDB.java
@@ -8,7 +8,7 @@ public class ClienteFakeDB extends BaseFakeDB<Cliente> {
     @Override
     protected void preencherDados() {
         this.tabela = new ArrayList<>();
-        tabela.add(new Cliente(1, "João Silva", "joao@exemplo.com", "(11) 99999-9999"));
-        tabela.add(new Cliente(2, "Maria Oliveira", "maria@exemplo.com", "(11) 98888-8888"));
+        tabela.add(new Cliente(1, "João Silva", "joao@uniderp.com", "(11) 99999-9999"));
+        tabela.add(new Cliente(2, "Maria Oliveira", "maria@uniderp.com", "(11) 98888-8888"));
     }
 }
